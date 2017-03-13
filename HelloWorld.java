a7c250398acad8f8023b0cc1cdf2d734c72321c2
Mindaugas <mindaugas.timonovas@gmail.com>
2017-03-13 14:08:29 +0200

    mmmm...

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,9 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("HELLO UGLY WORLD!\nThe World is not ugly!");
 
+		Date date = new Date();
+		System.out.println(date.toString());
+
 		Input in = new Input();
 		in.Enter();
 	}
