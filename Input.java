6ebc1271abc8833c13613b262a4290bbefa3e018
Mindaugas <mindaugas.timonovas@gmail.com>
2017-03-13 13:03:24 +0200

    update

---
+++ Input.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Input{
+	
+	    public static void main(String[] args) {
+	    	Scanner in = new Scanner(System.in);	    	
+	    	System.out.println("Ivesk savo varda:");
+	    	String name = in.nextLine();
+	    	
+	    System.out.println("Hello"+name);
+	    }
+	}
