153ffd168dc39fadd370e5da05bb5c4023d403ee
Mindaugas <mindaugas.timonovas@gmail.com>
2017-03-13 12:47:18 +0200

    update

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,12 +1,5 @@
-package firstlesson;
-
-/**
- * @author zwaclaw
- *
- */
-
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("HELLO UGLY WORLD!"),
+		System.out.println("HELLO UGLY WORLD!");
 	}
 }
