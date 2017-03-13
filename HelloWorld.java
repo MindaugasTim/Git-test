cb829118d47122cbcc9566e9562380b4cf1843af
Mindaugas <mindaugas.timonovas@gmail.com>
2017-03-13 13:23:15 +0200

    Hello vardas

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("HELLO UGLY WORLD!\nThe World is not ugly!");
+
+		Input in = new Input();
+		in.Enter();
 	}
 }
